db867d7a35c6366ccc96ae7785167bbac94f3980
Jaydeep Sadhu <jaydeepsadhu2005@gmail.com>
2025-10-10 11:36:14 +0530

    Initial commit - MERN fullstack

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS v3 Working 
+      </h1>
+      <p className="text-gray-500 mt-2">You fixed the warning!</p>
+    </div>
+  );
+}
+
+export default App;
